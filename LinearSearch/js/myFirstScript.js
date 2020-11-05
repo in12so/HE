c9c4ce25d8629584071358f4f18af4c66633ca6b
in12so <73948915+in12so@users.noreply.github.com>
2020-11-05 09:26:59 +0000

    Update myFirstScript.js
    Addition of item to array

--- LinearSearch/js/myFirstScript.js
+++ LinearSearch/js/myFirstScript.js
@@ -1,8 +1,9 @@
 
-var arr = [3,4,5,7,8,9,12];  
+var arr = [3,4,5,7,8,9,12,15];  
 var arrayLength = arr.length;
 var numberChoice = parseInt(prompt("Please enter a number to search for :"));
 var addressNumber;
+
 var addressFound = false;
 
 for(var i = 0;i<arrayLength;i++)
